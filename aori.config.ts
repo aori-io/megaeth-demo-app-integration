7ed4b145e27a87565c47cc72fc3fab027c1b7714
Andrew Duliba <a_duliba@protonmail.com>
2026-03-17 15:28:11 -0400

    0.1.4

--- aori.config.ts
+++ aori.config.ts
@@ -86,21 +86,16 @@ export const aoriConfig: AoriSwapWidgetConfig = {
     supportedInputTokens: [
       { chainId: 1, address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48' },     // USDC Ethereum
       { chainId: 1, address: '0xdAC17F958D2ee523a2206206994597C13D831ec7' },     // USDT Ethereum
-      { chainId: 1, address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' },     // ETH Ethereum
       { chainId: 10, address: '0x0b2c639c533813f4aa9d7837caf62653d097ff85' },    // USDC Optimism
       { chainId: 10, address: '0x01bFF41798a0BcF287b996046Ca68b395DbC1071' },    // USDT0 Optimism
-      { chainId: 10, address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' },    // ETH Optimism
       { chainId: 143, address: '0x754704bc059f8c67012fed69bc8a327a5aafb603' },   // USDC Monad
       { chainId: 143, address: '0x6873213455565656565656565656565656565656' },   // USDT0 Monad
       { chainId: 8453, address: '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913' },  // USDC Base
-      { chainId: 8453, address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' },  // ETH Base
       { chainId: 42161, address: '0xaf88d065e77c8cc2239327c5edb3a432268e5831' }, // USDC Arbitrum
       { chainId: 42161, address: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9' }, // USDT0 Arbitrum
-      { chainId: 42161, address: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' }, // ETH Arbitrum
     ],
     supportedOutputTokens: [
       { chainId: 4326, address: '0xfafddbb3fc7688494971a79cc65dca3ef82079e7' }, // USDC MegaETH
-      { chainId: 4326, address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee' }, // ETH MegaETH
     ],
     supportedInputChains: [1, 10, 143, 8453, 42161],
     supportedOutputChains: [4326],
@@ -111,7 +106,7 @@ export const aoriConfig: AoriSwapWidgetConfig = {
     enabledChains: [1, 10, 56, 143, 4326, 8453, 42161],
     lockBase: false,
     lockQuote: false,
-    disableInverting: true,
+    disableInverting: false,
   },
   appearance: {
     widgetType: 'default',
@@ -123,7 +118,7 @@ export const aoriConfig: AoriSwapWidgetConfig = {
     quoteLoaderVariant: 'expanded',
     fillContainer: false,
     hideBorder: true,
-    walletButtonEnabled: false,
+    walletButtonEnabled: true,
   },
   settings: {
     defaultSlippage: 0.01,
